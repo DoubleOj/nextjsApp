68a6a50c09dcd6a7c72e76b1680e9e2d89353084
Jonathan Jacob <yonatonjacob@gmail.com>
2023-07-17 12:15:43 -0400

    new file:   components/layouts.js 	modified:   pages/posts/first-post.js

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,21 +1,25 @@
 import Link from "next/link"
 import Image from "next/image"
 import Head from "next/head"
+import Layout from "../../components/layouts"
+
 export default function FirstPost(){
     return <>
-        <Head>
-            <title>First Post</title>
-        </Head>
-        <h1>First Post</h1>
-        {/* <Image 
-            src="/images/profile-pic.jpg"
-            height={144}
-            width={144}
-            alt="Profile Picture"
-        ></Image> */}
-        <h2>
-            <Link href="/">Back to Home</Link>
-        </h2>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+            {/* <Image 
+                src="/images/profile-pic.jpg"
+                height={144}
+                width={144}
+                alt="Profile Picture"
+            ></Image> */}
+            <h2>
+                <Link href="/">Back to Home</Link>
+            </h2>
+        </Layout>
     </>
 }
 
