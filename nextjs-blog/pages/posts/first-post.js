ecf283e8e9545e01a091636faf3ef95bc702f777
Jonathan Jacob <yonatonjacob@gmail.com>
2023-07-18 12:18:02 -0400

    still more todo

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,25 +1,20 @@
-import Link from "next/link"
-import Image from "next/image"
-import Head from "next/head"
-import Layout from "../../components/layouts"
+import Head from "next/head";
+import Layout, { siteTitle } from "../../components/layouts";
+import utilStyles from "../../components/utils.module.css";
 
-export default function FirstPost(){
-    return <>
-        <Layout>
-            <Head>
-                <title>First Post</title>
-            </Head>
-            <h1>First Post</h1>
-            {/* <Image 
-                src="/images/profile-pic.jpg"
-                height={144}
-                width={144}
-                alt="Profile Picture"
-            ></Image> */}
-            <h2>
-                <Link href="/">Back to Home</Link>
-            </h2>
-        </Layout>
-    </>
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
 }
-
